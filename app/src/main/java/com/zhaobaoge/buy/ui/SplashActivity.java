53635f68be318d19507562d719e3b9d5e988ef4d
skw <kewei.shi@mail.tipcat.com>
2018-11-02 16:11:53 +0800

    a

---
+++ app/src/main/java/com/zhaobaoge/buy/ui/SplashActivity.java
@@ -0,0 +1,54 @@
+package com.zhaobaoge.buy.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+
+import com.zhaobaoge.buy.R;
+import com.zhaobaoge.buy.ui.index.IndexActivity;
+
+/**
+ * Created by shikewei on 2018/11/2.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 取消标题
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 取消状态栏
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        final View view = new View(this);
+        view.setBackgroundResource(R.drawable.splash);
+        setContentView(view);
+        // 渐变展示启动屏
+        AlphaAnimation aa = new AlphaAnimation(0.5f, 1.0f);
+        aa.setDuration(3000);
+        view.startAnimation(aa);
+        aa.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                redirectTo();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+        });
+    }
+
+    private void redirectTo() {
+        startActivity(new Intent(this, IndexActivity.class));
+        finish();
+    }
+}
